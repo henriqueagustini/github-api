8f7cbe2007be32af6137e5bc54a6527f3ddccd03
Henrique Agustini <henrique80@gmail.com>
2022-07-07 23:37:54 -0300

    funcionalidades adicionadas

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
 import React from "react";
 import Layout from "./components/layout";
+import NoSearch from "./components/no-search";
+import Profile from "./components/profile";
+import Repositories from "./components/repositories";
+import useGithub from "./hooks/github-hooks";
 
-function App() {
+const App = () => {
+  const { githubState } = useGithub();
   return (
-    <main>
-      <Layout>
-        <div>
-          <img
-            source="https://avatars.githubusercontent.com/u/88353028?v=4"
-            alt="Avatar of user"
-          />
-          <h1>Henrique Agustini</h1>
-          <h3>Username:</h3>
-          <span>benits</span>
-        </div>
-        <div>
-          <div>
-            <h4>Followers</h4>
-            <span>5</span>
-          </div>
-          <div>
-            <h4>Starreds</h4>
-            <span>5</span>
-          </div>
-          <div>
-            <h4>Followings</h4>
-            <span>5</span>
-          </div>
-        </div>
-        <div>Repositories</div>
-        <div>Starreds</div>
-      </Layout>
-    </main>
+    <Layout>
+      {githubState.hasUser ? (
+        <>
+          {githubState.loading ? (
+            <p>Loading</p>
+          ) : (
+            <>
+              <Profile />
+              <Repositories />
+            </>
+          )}
+        </>
+      ) : (
+        <NoSearch />
+      )}
+    </Layout>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import ReactDOM from "react-dom";
+import Providers from "./providers.js";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Providers />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
